e33628839dc67b04be3e9d59a94368d75e098ddf
pankajj <pankaj0619@gmail.com>
2018-05-10 09:37:42 +0530

    Add PinView 2

---
+++ app/src/main/java/latest/pankaj/utility/credit_card_view/util/AndroidUtils.java
@@ -0,0 +1,16 @@
+package latest.pankaj.utility.credit_card_view.util;
+
+import android.os.Build;
+
+/**
+ * Created by Gregory on 6/30/2015.
+ */
+public class AndroidUtils {
+
+    //SDK 14 Version 4.0
+    public final static boolean icsOrBetter(){
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+    }
+
+    private AndroidUtils(){}
+}
