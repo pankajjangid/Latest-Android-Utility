a9305e94711be925690151b94424d4b5db2ed3bc
pankajj <pankaj0619@gmail.com>
2018-05-10 17:51:18 +0530

    Rings View Added

---
+++ app/src/main/java/latest/pankaj/utility/rings_view/RingsViewExampleActivity.java
@@ -0,0 +1,15 @@
+package latest.pankaj.utility.rings_view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import latest.pankaj.utility.R;
+
+public class RingsViewExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rings_view_example);
+    }
+}
